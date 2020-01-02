2cc8c0cd7f38a6f84f23398c86574688d00c38a9
Robert Long <longrob604@gmail.com>
2020-01-02 11:18:53 +0000

    Start Chapter 5

--- hutton/refex01.hs
+++ hutton/refex01.hs
@@ -1,11 +1,40 @@
 {- 
 Author: Robert Long
 
-Refresher exercises from Hutton
+Haskell Refresher exercises from Hutton
 
 -}
 
--- chapter 2
+-- Chapter 3
 --
--- Exercise 
+-- Exercise 3
+
 last' xs = head (reverse xs)
+
+-- Chapter 4 - function definitions
+-- 
+-- Exercise 1
+
+halve xs = (take n xs, drop n xs)
+           where n = length xs `div` 2 
+
+-- Exercise 2
+
+third xs = head (tail (tail xs))
+--third xs = xs !! 2
+--third (_:_:x:_) = x
+
+-- Exercise 3
+
+--safetail xs = if null xs then [] else tail xs  
+-- using conditional expressions
+
+--safetail xs | null xs = []  -- using 
+--            | otherwise = tail xs
+-- using guarded equations
+
+safetail [] = []
+safetail (_:xs) = xs
+-- using pattern matching
+
+-- Chapter 5 - list comprehensions
